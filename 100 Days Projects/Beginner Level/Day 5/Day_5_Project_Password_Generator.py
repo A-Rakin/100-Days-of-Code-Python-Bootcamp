ef7b8ac1f6f5749092cd42dfe1ac7e13920edee0
Abrar Rakin <144883380+A-Rakin@users.noreply.github.com>
2025-06-26 18:49:02 +0600

    Update Day_5_Project_Password_Generator.py

--- 100 Days Projects/Beginner Level/Day 5/Day_5_Project_Password_Generator.py
+++ 100 Days Projects/Beginner Level/Day 5/Day_5_Project_Password_Generator.py
@@ -4,19 +4,19 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+letters = int(input("How many letters would you like in your password?\n")) 
+symbols = int(input(f"How many symbols would you like?\n"))
+numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(1, nr_letters + 1):
+for char in range(1, letters + 1):
   password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
+for char in range(1, symbols + 1):
   password_list += random.choice(symbols)
 
-for char in range(1, nr_numbers + 1):
+for char in range(1, numbers + 1):
   password_list += random.choice(numbers)
   
 password = ""
